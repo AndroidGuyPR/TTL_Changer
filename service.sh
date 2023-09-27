3ec3c7ffdab657c6cdf7de60d046fe9198647d48
AndroidGuyPR <146151303+AndroidGuyPR@users.noreply.github.com>
2023-09-26 22:19:53 -0400

    Update service.sh
    1. Using sysctl command instead of directly writing to /proc/sys files for setting TTL values.
    
    2. Using ip command instead of ifconfig for retrieving network interfaces.
    
    3. Checking if the "TTL" target is already present in the iptables mangle table to avoid duplicate rules.
    
    4. Adjusting the indentation and adding line breaks to improve readability.

--- service.sh
+++ service.sh
@@ -3,30 +3,29 @@ sleep 15
 
 set_ttl_65()
 {
-  echo 65 > /proc/sys/net/ipv4/ip_default_ttl
-  echo 65 > /proc/sys/net/ipv6/conf/all/hop_limit
-  echo 1 > /proc/sys/net/ipv4/ip_forward
-  echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
-  echo 1 > /proc/sys/net/ipv4/ip_dynaddr
-  echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+  sysctl net.ipv4.ip_default_ttl=65
+  sysctl net.ipv6.conf.all.hop_limit=65
+  sysctl net.ipv4.ip_forward=1
+  sysctl net.ipv4.conf.all.forwarding=1
+  sysctl net.ipv4.ip_dynaddr=1
+  sysctl net.ipv6.conf.all.disable_ipv6=1
 }
+
 mark_traffic_ttl()
 {  
-for i in $(ifconfig -a | sed 's/[ \t].*//;/^$/d'); do (iptables -t mangle -A PREROUTING -i $i -j TTL --ttl-set 65); done
+  for i in $(ip -o link show | awk -F': ' '{print $2}'); do (iptables -t mangle -A PREROUTING -i $i -j TTL --ttl-set 65); done
 }
+
 settings put global tether_dun_required 0
-    
 settings put system tether_entitlement_check_state 0
 
-if [ -x "$(command -v iptables)" ]
-then
-	if [ $(grep -q TTL /proc/net/ip_tables_targets) ]
-	then
-		for i in $(ifconfig -a | sed 's/[ \t].*//;/^$/d'); do (iptables -t mangle -A POSTROUTING -i $i -j TTL --ttl-set 65); done
-	else
-	set_ttl_65
-	mark_traffic_ttl
-	fi
+if [ -x "$(command -v iptables)" ]; then
+  if iptables -L -t mangle --line-numbers | grep -q TTL; then
+    for i in $(ip -o link show | awk -F': ' '{print $2}'); do (iptables -t mangle -A POSTROUTING -i $i -j TTL --ttl-set 65); done
+  else
+    set_ttl_65
+    mark_traffic_ttl
+  fi
 else
-	set_ttl_65
+  set_ttl_65
 fi
